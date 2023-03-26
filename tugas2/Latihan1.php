2ab32569434f1202176fc473d12ddafbff53e3de
Zxall <Arfianrizal54@gmaill.com>
2023-03-26 12:53:28 +0700

    menambah 3 file baru

---
+++ tugas2/Latihan1.php
@@ -0,0 +1,21 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "Selamat Datang.. selamat belajar Web programing";
+		//$this->load->view('view-latihan');
+	}
+
+	public function penjumlahan($n1, $n2)
+	{
+     
+        $this->load->model('Model_latihan');
+
+		$data['nilai1'] = $n1;
+		$data['nilai2'] = $n2;
+		$data['hasil'] = $this->Model_latihan->jumlah($n1, $n2);
+
+		$this->load->view('view-latihan', $data);
+	}
+}
